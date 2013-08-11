3a7e317419d3857c0a6a3b88ec1e9b8dfb625acd
Torbjørn Kvåle <torbjoernkvaale@gmail.com>
2013-08-11 16:15:05 +0200

    a more secure random function

--- api.php
+++ api.php
@@ -5,15 +5,21 @@ header("Cache-Control: s-maxage=0, max-age=0, must-revalidate, no-cache");
 #https://skydrive.live.com/?cid=3732e80b128d016f&id=3732E80B128D016F%213584
 # remove digits " (\d*)$"
 
-$str = file_get_contents('words/en.txt'); //Take the contents from the file to the variable
-$result = explode("\n",$str); //Split it by ','
+$str = file_get_contents('words/en.txt');
+$result = explode("\n",$str);
 
 
-
-$w1 = $result[array_rand($result)];
+$w1 = $result[secure_random(count($result))];
 
 header('Content-Type: text/plain; charset=utf-8');
 header('Access-Control-Allow-Origin: *');
 echo $w1;
 
+
+function secure_random($max){
+    do{
+        $result = floor($max*(hexdec(bin2hex(openssl_random_pseudo_bytes(4)))/0xffffffff));
+    }while($result == $max);
+    return $result;
+}
 ?>
